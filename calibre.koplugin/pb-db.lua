95ef0002e05db294da18aeb174c089986341bd8e
Oleh Tiuriakov <152391484+reuerendo@users.noreply.github.com>
2025-08-19 15:44:03 +0200

    Add files via upload

--- calibre.koplugin/pb-db.lua
+++ calibre.koplugin/pb-db.lua
@@ -23,6 +23,7 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
     local db_path = "/mnt/ext1/system/explorer-3/explorer-3.db"
     collections_lookup_name = G_reader_settings:readSetting("collections_name")
     read_lookup_name = G_reader_settings:readSetting("read_name")
+	read_date_lookup_name = G_reader_settings:readSetting("read_date_name")
     favorite_lookup_name = G_reader_settings:readSetting("favorite_name")
     
     -- Добавляем отладочную информацию
@@ -100,7 +101,7 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
     local folder_id, book_id
     local folder = filename:match("(.+)/[^/]+$")
     local storage_id = get_storage_id(filename)
-		logger.info("stotage_id:", storage_id)
+	logger.info("storage_id:", storage_id)
     local file_name = filename:match("/([^/]+)$")
     local file_ext = file_name:match("%.([^%.]+)$")
 
@@ -126,6 +127,7 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
         return
     end
 
+    -- Обработка папки
     local insert_folder_sql = [[
         INSERT INTO folders (storageid, name) 
         VALUES (?, ?) 
@@ -173,177 +175,197 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
     end
 
     local author = table.concat(arg.metadata.authors or {}, ", ")
-    local first_author = arg.metadata.authors and arg.metadata.authors[1] or ""
-	
-	-- Проверяем существование книги
-	local check_book_sql = [[
-		SELECT id FROM books_impl 
-		WHERE title = ? AND author = ?;
-	]]
-
-	local check_book_stmt = db:prepare(check_book_sql)
-	check_book_stmt:bind1(1, title)
-	check_book_stmt:bind1(2, author)
-	local existing_book = check_book_stmt:step()
-	check_book_stmt:close()
-
-	if type(existing_book) == "table" then
-		-- Обновляем существующую книгу
-		book_id = existing_book[1]
-		local update_book_sql = [[
-			UPDATE books_impl SET
-				series = ?,
-				numinseries = ?,
-				size = ?,
-				isbn = ?,
-				updated = ?,
-				ts_added = ?
-			WHERE id = ?;
-		]]
-		
-		local update_book_stmt = db:prepare(update_book_sql)
-		update_book_stmt:bind1(1, safeToString(arg.metadata.series))
-		update_book_stmt:bind1(2, tonumber(safeToString(arg.metadata.series_index)) or 0)
-		update_book_stmt:bind1(3, arg.metadata.size)
-		update_book_stmt:bind1(4, safeToString(arg.metadata.isbn))
-		update_book_stmt:bind1(5, current_timestamp)
-		update_book_stmt:bind1(6, current_timestamp)
-		update_book_stmt:bind1(7, book_id)
-
-		if update_book_stmt:step() ~= SQ3.DONE then
-			logger.info("Ошибка при обновлении книги")
-			db:exec("ROLLBACK")
-			safe_db_close()
-			return
-		end
-		update_book_stmt:close()
-	else
-	
-		-- Добавляем новую книгу (оставляем оригинальный код вставки)
-    
-    local book_sql = [[
-        INSERT INTO books_impl (
-            title, first_title_letter, author, firstauthor, 
-            first_author_letter, series, numinseries, size, 
-            isbn, sort_title, creationtime, updated, 
-            ts_added, hidden
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ]]
-    
-    local book_stmt = db:prepare(book_sql)
-    if not book_stmt then
-        logger.info("Ошибка: не удалось подготовить SQL-запрос для книги!")
-        db:exec("ROLLBACK")
-        safe_db_close()
-        return
-    end
-
     local title = safeToString(arg.metadata.title)
     
-    book_stmt:bind1(1, title)
-    book_stmt:bind1(2, getFirstLetter(title))
-    book_stmt:bind1(3, author)
-    book_stmt:bind1(4, arg.metadata.author_sort)
-    book_stmt:bind1(5, getFirstLetter(arg.metadata.author_sort))
-    book_stmt:bind1(6, safeToString(arg.metadata.series))
-    book_stmt:bind1(7, tonumber(safeToString(arg.metadata.series_index)) or 0)
-    book_stmt:bind1(8, arg.metadata.size)
-    book_stmt:bind1(9, safeToString(arg.metadata.isbn))
-    book_stmt:bind1(10, title)
-    book_stmt:bind1(11, 0)
-    book_stmt:bind1(12, 0)
-    book_stmt:bind1(13, current_timestamp)
-    book_stmt:bind1(14, 0)
-
-    if book_stmt:step() ~= SQ3.DONE then
-        logger.info("Ошибка при добавлении книги")
-        db:exec("ROLLBACK")
-        safe_db_close()
-        return
-    end
-
-    book_id = db:rowexec("SELECT last_insert_rowid()")
-    book_stmt:close()
-
-    if not book_id then
-        logger.info("Ошибка: не удалось получить ID книги")
-        db:exec("ROLLBACK")
-        safe_db_close()
-        return
-    end
-	end
-	
-	-- Проверяем существование файла
+	-- Сначала проверяем существование файла
 	local check_file_sql = [[
-		SELECT id FROM files 
+		SELECT id, book_id FROM files 
 		WHERE filename = ? AND folder_id = ?;
 	]]
 
 	local check_file_stmt = db:prepare(check_file_sql)
+	if not check_file_stmt then
+        logger.info("Ошибка: не удалось подготовить запрос проверки файла")
+        db:exec("ROLLBACK")
+        safe_db_close()
+        return
+    end
+    
 	check_file_stmt:bind1(1, file_name)
 	check_file_stmt:bind1(2, folder_id)
 	local existing_file = check_file_stmt:step()
 	check_file_stmt:close()
 
 	if type(existing_file) == "table" then
-		-- Обновляем существующий файл
+		-- Файл существует, получаем book_id
+		local file_id = existing_file[1]
+		book_id = existing_file[2]
+		logger.info("Найден существующий файл, file_id:", file_id, "book_id:", book_id)
+		
+		-- Обновляем информацию о файле
 		local update_file_sql = [[
 			UPDATE files SET
-				book_id = ?,
 				size = ?,
 				modification_time = ?
-			WHERE filename = ? AND folder_id = ?;
+			WHERE id = ?;
 		]]
 		
 		local update_file_stmt = db:prepare(update_file_sql)
-		update_file_stmt:bind1(1, book_id)
-		update_file_stmt:bind1(2, file_stats.size)
-		update_file_stmt:bind1(3, file_stats.modification)
-		update_file_stmt:bind1(4, file_name)
-		update_file_stmt:bind1(5, folder_id)
+		if not update_file_stmt then
+			logger.info("Ошибка: не удалось подготовить запрос обновления файла")
+			db:exec("ROLLBACK")
+			safe_db_close()
+			return
+		end
+		
+		update_file_stmt:bind1(1, file_stats.size)
+		update_file_stmt:bind1(2, file_stats.modification)
+		update_file_stmt:bind1(3, file_id)
 
 		if update_file_stmt:step() ~= SQ3.DONE then
 			logger.info("Ошибка при обновлении файла")
 			success = false
 		end
 		update_file_stmt:close()
+		
+		-- Обновляем информацию о книге
+		local update_book_sql = [[
+			UPDATE books_impl SET
+				title = ?,
+				first_title_letter = ?,
+				author = ?,
+				firstauthor = ?,
+				first_author_letter = ?,
+				series = ?,
+				numinseries = ?,
+				size = ?,
+				isbn = ?,
+				sort_title = ?,
+				updated = ?,
+				ts_added = ?
+			WHERE id = ?;
+		]]
+		
+		local update_book_stmt = db:prepare(update_book_sql)
+		if not update_book_stmt then
+			logger.info("Ошибка: не удалось подготовить запрос обновления книги")
+			db:exec("ROLLBACK")
+			safe_db_close()
+			return
+		end
+		
+		update_book_stmt:bind1(1, title)
+		update_book_stmt:bind1(2, getFirstLetter(title))
+		update_book_stmt:bind1(3, author)
+		update_book_stmt:bind1(4, arg.metadata.author_sort)
+		update_book_stmt:bind1(5, getFirstLetter(arg.metadata.author_sort))
+		update_book_stmt:bind1(6, safeToString(arg.metadata.series))
+		update_book_stmt:bind1(7, tonumber(safeToString(arg.metadata.series_index)) or 0)
+		update_book_stmt:bind1(8, arg.metadata.size)
+		update_book_stmt:bind1(9, safeToString(arg.metadata.isbn))
+		update_book_stmt:bind1(10, title)
+		update_book_stmt:bind1(11, current_timestamp)
+		update_book_stmt:bind1(12, current_timestamp)
+		update_book_stmt:bind1(13, book_id)
+
+		if update_book_stmt:step() ~= SQ3.DONE then
+			logger.info("Ошибка при обновлении книги")
+			success = false
+		end
+		update_book_stmt:close()
+		logger.info("Обновлена существующая книга, book_id:", book_id)
+		
 	else
-	
-		-- Добавляем новый файл (оставляем оригинальный код вставки)
-	
-    local files_sql = [[
-        INSERT INTO files (
-            storageid, folder_id, book_id, filename, 
-            size, modification_time, ext
-        ) VALUES (?, ?, ?, ?, ?, ?, ?)
-    ]]
-    
-    local files_stmt = db:prepare(files_sql)
-    if not files_stmt then
-        logger.info("Ошибка: не удалось подготовить SQL-запрос для файла!")
-        db:exec("ROLLBACK")
-        safe_db_close()
-        return
-    end
+		-- Файл не существует, создаем новую запись книги и файла
+		logger.info("Файл не найден, создаем новые записи")
+		
+		-- Добавляем новую книгу
+		local book_sql = [[
+			INSERT INTO books_impl (
+				title, first_title_letter, author, firstauthor, 
+				first_author_letter, series, numinseries, size, 
+				isbn, sort_title, creationtime, updated, 
+				ts_added, hidden
+			) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+		]]
+		
+		local book_stmt = db:prepare(book_sql)
+		if not book_stmt then
+			logger.info("Ошибка: не удалось подготовить SQL-запрос для книги!")
+			db:exec("ROLLBACK")
+			safe_db_close()
+			return
+		end
 
-    files_stmt:bind1(1, storage_id)
-    files_stmt:bind1(2, folder_id)
-    files_stmt:bind1(3, book_id)
-    files_stmt:bind1(4, file_name)
-    files_stmt:bind1(5, file_stats.size)
-    files_stmt:bind1(6, file_stats.modification)
-    files_stmt:bind1(7, file_ext)
+		book_stmt:bind1(1, title)
+		book_stmt:bind1(2, getFirstLetter(title))
+		book_stmt:bind1(3, author)
+		book_stmt:bind1(4, arg.metadata.author_sort)
+		book_stmt:bind1(5, getFirstLetter(arg.metadata.author_sort))
+		book_stmt:bind1(6, safeToString(arg.metadata.series))
+		book_stmt:bind1(7, tonumber(safeToString(arg.metadata.series_index)) or 0)
+		book_stmt:bind1(8, arg.metadata.size)
+		book_stmt:bind1(9, safeToString(arg.metadata.isbn))
+		book_stmt:bind1(10, title)
+		book_stmt:bind1(11, 0)
+		book_stmt:bind1(12, 0)
+		book_stmt:bind1(13, current_timestamp)
+		book_stmt:bind1(14, 0)
 
-    if files_stmt:step() ~= SQ3.DONE then
-        logger.info("Ошибка при добавлении файла")
-        success = false
-    end
-    files_stmt:close()
+		if book_stmt:step() ~= SQ3.DONE then
+			logger.info("Ошибка при добавлении книги")
+			db:exec("ROLLBACK")
+			safe_db_close()
+			return
+		end
+
+		book_id = db:rowexec("SELECT last_insert_rowid()")
+		book_stmt:close()
+
+		if not book_id then
+			logger.info("Ошибка: не удалось получить ID книги")
+			db:exec("ROLLBACK")
+			safe_db_close()
+			return
+		end
+		logger.info("Создана новая книга, book_id:", book_id)
+		
+		-- Добавляем новый файл
+		local files_sql = [[
+			INSERT INTO files (
+				storageid, folder_id, book_id, filename, 
+				size, modification_time, ext
+			) VALUES (?, ?, ?, ?, ?, ?, ?)
+		]]
+		
+		local files_stmt = db:prepare(files_sql)
+		if not files_stmt then
+			logger.info("Ошибка: не удалось подготовить SQL-запрос для файла!")
+			db:exec("ROLLBACK")
+			safe_db_close()
+			return
+		end
+
+		files_stmt:bind1(1, storage_id)
+		files_stmt:bind1(2, folder_id)
+		files_stmt:bind1(3, book_id)
+		files_stmt:bind1(4, file_name)
+		files_stmt:bind1(5, file_stats.size)
+		files_stmt:bind1(6, file_stats.modification)
+		files_stmt:bind1(7, file_ext)
+
+		if files_stmt:step() ~= SQ3.DONE then
+			logger.info("Ошибка при добавлении файла")
+			success = false
+		end
+		files_stmt:close()
+		logger.info("Создан новый файл")
 	end
 	
 	if success then
 		-- Обработка коллекций
-		if arg.metadata.user_metadata[collections_lookup_name] and 
+		if arg.metadata.user_metadata and 
+		   arg.metadata.user_metadata[collections_lookup_name] and 
 		   arg.metadata.user_metadata[collections_lookup_name]["#value#"] then
 			local collections = arg.metadata.user_metadata[collections_lookup_name]["#value#"]
 			logger.info("Начало обработки коллекций:", collections)
@@ -466,9 +488,8 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
 		end
 	end
 
-    if success then
-	
-        -- Проверяем наличие меток в метаданных
+	if success then
+		-- Проверяем наличие меток в метаданных
 		local function GetCurrentProfileId()
 			local profile_name = inkview.GetCurrentProfile()
 			if profile_name == nil then
@@ -491,7 +512,68 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
 		local has_favorite = arg.metadata.user_metadata 
 			and arg.metadata.user_metadata[favorite_lookup_name] 
 			and arg.metadata.user_metadata[favorite_lookup_name]["#value#"] == true
-        
+
+		-- Get read date if available
+		local completed_date = nil
+		if has_read and arg.metadata.user_metadata and arg.metadata.user_metadata[read_date_lookup_name] then
+			local date_value = arg.metadata.user_metadata[read_date_lookup_name]["#value#"]
+			logger.info("Raw date from Calibre:", date_value) -- Debug: original date from Calibre
+			
+			if date_value and date_value ~= "" then
+				-- Convert calibre datetime string to YYYY-MM-DD format
+				-- Calibre format: "YYYY-MM-DDTHH:MM:SS+00:00" (UTC)
+				local year, month, day, hour, min, sec = date_value:match("(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)")
+				logger.info("Parsed components:", year, month, day, hour, min, sec) -- Debug: parsed components
+				
+				if year and month and day and hour and min and sec then
+					-- Convert UTC to local time
+					local utc_time = os.time({
+						year = tonumber(year),
+						month = tonumber(month),
+						day = tonumber(day),
+						hour = tonumber(hour),
+						min = tonumber(min),
+						sec = tonumber(sec)
+					})
+					logger.info("UTC timestamp:", utc_time) -- Debug: UTC timestamp
+					
+					-- Get local time
+					local local_time = os.date("*t", utc_time)
+					logger.info("Local time components:", local_time.year, local_time.month, local_time.day) -- Debug: local time components
+					
+					completed_date = string.format("%04d-%02d-%02d", 
+						local_time.year, local_time.month, local_time.day)
+					logger.info("Final converted date (UTC->Local):", completed_date)
+				else
+					logger.info("Failed to parse date components from:", date_value)
+				end
+			else
+				logger.info("Date value is empty or nil")
+			end
+		else
+			logger.info("Read date metadata not found or conditions not met")
+		end
+		
+		-- Передаем данные в DocSettings вместо базы данных
+		if has_read or has_favorite or completed_date then
+			local DocSettings = require("docsettings")
+			local doc_settings = DocSettings:open(filename)
+			
+			if has_read then
+				logger.info("Устанавливаем метку 'прочитано' в DocSettings")
+				doc_settings:saveSetting("summary", {status = "complete"})
+			end
+			
+			if completed_date then
+				logger.info("Устанавливаем дату завершения в DocSettings:", completed_date)
+				doc_settings:saveSetting("summary", {status = "complete", modified = completed_date})
+			end
+			
+			doc_settings:flush()
+			logger.info("Метки сохранены в DocSettings")
+		end
+		
+		-- Обрабатываем избранное в базе данных (если нужно)
         if has_read or has_favorite then
 			local completed = has_read and 1 or 0
 			local favorite = has_favorite and 1 or 0
@@ -507,6 +589,7 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
                 success = false
             else
                 select_settings_stmt:bind1(1, book_id)
+                select_settings_stmt:bind1(2, profile_id)
                 local settings_row = select_settings_stmt:step()
                 select_settings_stmt:close()
 
@@ -526,7 +609,7 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
                         update_settings_stmt:bind1(1, completed)
                         update_settings_stmt:bind1(2, favorite)
                         update_settings_stmt:bind1(3, book_id)
-						insert_settings_stmt:bind1(4, profile_id)
+                        update_settings_stmt:bind1(4, profile_id)
                         
                         if update_settings_stmt:step() ~= SQ3.DONE then
                             logger.info("Ошибка при обновлении настроек")
@@ -547,7 +630,7 @@ function PocketBookDBHandler:saveBookToDatabase(arg, filename)
                         success = false
                     else
                         insert_settings_stmt:bind1(1, book_id)
-						insert_settings_stmt:bind1(2, profile_id)
+                        insert_settings_stmt:bind1(2, profile_id)
                         insert_settings_stmt:bind1(3, completed)
                         insert_settings_stmt:bind1(4, favorite)
                         
